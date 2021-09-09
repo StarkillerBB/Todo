d900341c404cf52b2daf25c0fcde88c42eb3ff61
Benjamin Marius Lank <benj007d@elevcampus.dk>
2021-09-09 10:58:35 +0200

    Add project files.

---
+++ Model/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Model
+{
+    public class Todo
+    {
+
+        public Guid Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+
+    }
+}
